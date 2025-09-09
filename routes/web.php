79398a2d3ae94c7a290006a9b7186ee24c0ac459
GiaCng <147374173+GiaCng@users.noreply.github.com>
2025-09-09 07:37:24 +0700

    50% giao dien

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\clients\AboutController;
+use App\Http\Controllers\clients\BookingController;
+use App\Http\Controllers\clients\ContactController;
+use App\Http\Controllers\clients\DestinationController;
+use App\Http\Controllers\clients\HomeController;
+use App\Http\Controllers\clients\ToursController;
+use App\Http\Controllers\clients\TourDetailController;
+use App\Http\Controllers\clients\ServicesController;
+use App\Http\Controllers\clients\TravelGuidesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +22,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about',[AboutController::class, 'index'])->name('about');
+Route::get('/services', [ServicesController::class, 'index'])->name('services');
+Route::get('/tours', [ToursController::class, 'index'])->name('tours');
+Route::get('/tour-detail', [TourDetailController::class, 'index'])->name('tour-detail');
+Route::get('/destination', [DestinationController::class, 'index'])->name('destination');
+Route::get('/booking', [BookingController::class, 'index'])->name('booking');
+Route::get('/travel-guides', [TravelGuidesController::class, 'index'])->name('team');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
